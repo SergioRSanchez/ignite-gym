732c09ae4f44eb9793a79e8c66297c6baaf86cd1
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-04-25 16:06:49 -0300

    feat: started creation of SignIn screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import { Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from '@screens/SignIn';
 
 import { Loading } from '@components/Loading';
 
@@ -16,7 +18,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Text>Fonte carregada</Text> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
