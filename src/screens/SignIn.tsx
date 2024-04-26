856723df99ae1fdd48e24a7c2efec6ea7015854d
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-04-26 18:35:37 -0300

    feat: apply auth navigation to SignIn and SignUp screens

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+
+import { AuthNavigatorAuthProps } from '@routes/auth.routes';
 
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
@@ -7,6 +10,12 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorAuthProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  };
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} pb={16}>
@@ -52,6 +61,7 @@ export function SignIn() {
           <Button 
             title='Criar conta' 
             variant='outline'
+            onPress={handleNewAccount}
           />
         </Center>
       </VStack>
