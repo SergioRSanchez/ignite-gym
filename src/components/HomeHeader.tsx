37763154b7148ff756dc723c847e8341415f1132
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-04-27 15:22:57 -0300

    feat: finished HomeHeader component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,18 +1,23 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { UserPhoto } from './UserPhoto';
+
+import ProfileImage from '@assets/userPhotoDefault.png';
 
 export function HomeHeader() {
   return (
     <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
       <UserPhoto 
-        source={{ uri: 'https://github.com/SergioRSanchez.png' }}
+        // source={{ uri: 'https://github.com/SergioRSanchez.png' }}
+        source={ProfileImage}
         alt='Imagem do usuário'
         size={16}
         mr={4}
       />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color='gray.100' fontSize='md'>
           Olá,
         </Text>
@@ -21,6 +26,15 @@ export function HomeHeader() {
           Sérgio
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name='logout'
+          color='gray.200'
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   );
 }
