a95173f5518a28aa8f0036b56b3f40d10eed8310
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-04-27 16:30:48 -0300

    feat: finished Group component and implement in Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,28 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { HStack, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group 
-        name='costas'
-      />
+      <HStack>
+        <Group 
+          name='costa'
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name='ombro'
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   );
 }
