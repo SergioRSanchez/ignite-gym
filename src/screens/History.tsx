3baab42a9611ad128ac926f0da1fc598ca21a729
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-02 17:27:53 -0300

    feat: created HistoryCard component and use it in a Section List in History screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,38 @@
-import { VStack } from 'native-base';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
+import { useState } from 'react';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Remada curvada', 'Supino invertido', 'Supino declinado']
+    },
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios"/>
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>( 
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   );
 }
