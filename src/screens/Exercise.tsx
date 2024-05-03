911856a90312196203584318013bdd06ff97f9df
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-03 14:33:18 -0300

    feat: adjustments

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,6 +1,6 @@
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Heading, HStack, Icon, Text, VStack, Image, Box } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack, Image, Box, ScrollView } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 
 import { Button } from '@components/Button';
@@ -40,38 +40,40 @@ export function Exercise() {
         </HStack>
       </VStack>
 
-      <VStack p={8}>
-        <Image 
-          w='full'
-          h='80'
-          source={{ uri: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
-          alt='Imagem do exercício'
-          mb={3}
-          resizeMode='cover'
-          rounded='lg'
-          overflow='hidden'
-        />
+      <ScrollView>
+        <VStack p={8}>
+          <Image 
+            w='full'
+            h='80'
+            source={{ uri: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+            alt='Imagem do exercício'
+            mb={3}
+            resizeMode='cover'
+            rounded='lg'
+            overflow='hidden'
+          />
 
-        <Box bg='gray.600' rounded='md' pb={4} px={4}>
-          <HStack justifyContent='space-around' alignItems='center' mb={6} mt={5}>
-            <HStack>
-              <SeriesSvg />
-              <Text color='gray.200' ml={2}>
-                3 séries
-              </Text>
+          <Box bg='gray.600' rounded='md' pb={4} px={4}>
+            <HStack justifyContent='space-around' alignItems='center' mb={6} mt={5}>
+              <HStack>
+                <SeriesSvg />
+                <Text color='gray.200' ml={2}>
+                  3 séries
+                </Text>
+              </HStack>
+              
+              <HStack>
+                <RepetitionsSvg />
+                <Text color='gray.200' ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
             </HStack>
-            
-            <HStack>
-              <RepetitionsSvg />
-              <Text color='gray.200' ml={2}>
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
 
-          <Button title='Marcar como realizado' />
-        </Box>
-      </VStack>
+            <Button title='Marcar como realizado' />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
