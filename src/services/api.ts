549287f76c648c060b5043e4cff49854d85b9b56
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-12 11:51:57 -0300

    feat: created interceptor messages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
+import { AppError } from '@utils/AppError';
+
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BASE_URL,
 });
 
-api.interceptors.response.use((response) => {
-  console.log('INTERCEPTOR => ', response);
-  return response;
-}, (error) => {
-  console.log('INTERCEPTOR => ', error);
-  return Promise.reject(error);
+api.interceptors.response.use(response => response, error => {
+  if (error.response & error.response.data) {
+    return Promise.reject(new AppError(error.response.data.message));
+  } else {
+    return Promise.reject(new AppError('Erro no servidor. Tente novamente mais tarde.'));
+  }
 });
 
 export { api };
