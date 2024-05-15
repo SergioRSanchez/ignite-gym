b6673275db903a0a7d1f6890fae69a2480ac91bb
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-15 17:47:33 -0300

    feat: change routes if user sign in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,8 +10,6 @@ export function Routes() {
   const { colors } = useTheme();
 
   const { user } = useAuth();
-  
-  console.log(user)
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
@@ -19,7 +17,7 @@ export function Routes() {
   return (
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme}>
-        <AuthRoutes/>
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
