3556fc738590facc1de72ff63bdba70b4af8c5e6
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-22 18:00:29 -0300

    feat: name and user avatar in HomeHeader component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,15 +2,19 @@ import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import { useAuth } from '@hooks/useAuth';
+
 import { UserPhoto } from './UserPhoto';
 
 import ProfileImage from '@assets/userPhotoDefault.png';
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
       <UserPhoto 
-        source={{ uri: 'https://github.com/SergioRSanchez.png' }}
+        source={user.avatar ? { uri: user.avatar } : ProfileImage}
         alt='Imagem do usuário'
         size={16}
         mr={4}
@@ -22,7 +26,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color='gray.100' fontSize='md' fontFamily='heading'>
-          Sérgio
+          {user.name}
         </Heading>
       </VStack>
 
