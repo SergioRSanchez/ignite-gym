bc1bbf84e7dfafc042bca63ce7b0bb2ed5370d36
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-22 18:28:13 -0300

    feat: create signOut function

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
-import { storageUserSave, storageUserGet } from '@storage/storageUser';
+import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
 
 import { api } from '@services/api';
 
@@ -9,6 +9,7 @@ import { UserDTO } from '@dtos/UserDTO';
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
   isLoadingUserStorageData: boolean;
 }
 
@@ -34,6 +35,21 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       }
     } catch (error) {
       throw error;
+    }
+  }
+
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true);
+      
+      setUser({} as UserDTO);
+      await storageUserRemove();
+
+    } catch (error) {
+      throw error;
+
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -63,6 +79,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       value={{ 
         user, 
         signIn,
+        signOut,
         isLoadingUserStorageData
       }}
     >
