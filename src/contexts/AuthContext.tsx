561fe3b720a72bf39a30e2fdd5887dfb654e0f15
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-05-23 14:38:09 -0300

    feat: attaching token in requests

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
 import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser';
+import { storageAuthTokenSave } from '@storage/storageAuthToken';
 
 import { api } from '@services/api';
 
@@ -24,17 +25,35 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      setUser(userData);
+
+    } catch (error) {
+      throw error;
+
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
 
       if (data.user && data.token) {
-        setUser(data.user);
-        storageUserSave(data.user);
-
+        storageUserAndToken(data.user, data.token);
       }
+
     } catch (error) {
       throw error;
+      
     }
   }
 
