8c2d439d9d70d5242f5f285aaa6f9522f7ef7ad0
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-04 17:55:50 -0300

    feat: included return promise reject

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,6 +10,7 @@ api.interceptors.response.use(response => response, error => {
   if (error.response && error.response.data) {
     return Promise.reject(new AppError(error.response.data.message));
   } else {
+    return Promise.reject(error);
   }
 });
 
