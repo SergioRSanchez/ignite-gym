378c0e3ff74fa6519f1f28471baefc9fd5f60fe6
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-05 08:42:31 -0300

    feat: included token manager

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,31 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 import { AppError } from '@utils/AppError';
 
+type SignOut = () => void;
+
+type APIInstaceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
+
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BASE_URL,
-});
+}) as APIInstaceProps;
 
-api.interceptors.response.use(response => response, error => {
-  if (error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message));
-  } else {
-    return Promise.reject(error);
+api.registerInterceptTokenManager = signOut => {
+  const interceptTokenManager = api.interceptors.response.use(response => response, error => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(error);
+    }
+  });
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
   }
-});
+}
+
+
 
 export { api };
