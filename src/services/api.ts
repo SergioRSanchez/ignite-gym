592cabbe8e953f2994cd2508809d2dff519eb50b
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-06 17:51:13 -0300

    feat: placing request in queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,14 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosError } from 'axios';
 
 import { AppError } from '@utils/AppError';
 import { storageAuthTokenGet } from '@storage/storageAuthToken';
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+}
 
 type APIInstaceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -13,9 +18,12 @@ const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BASE_URL,
 }) as APIInstaceProps;
 
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
+
 api.registerInterceptTokenManager = signOut => {
   const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
-    if (requestError?.reponse?.status === 401) {
+    if (requestError?.response?.status === 401) {
       if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
         const { refresh_token } = await storageAuthTokenGet();
 
@@ -23,6 +31,25 @@ api.registerInterceptTokenManager = signOut => {
           signOut();
           return Promise.reject(requestError);
         }
+
+        const originalRequestConfig = requestError.config;
+
+        if (isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueue.push({
+              onSuccess: (token: string) => {
+                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}` };
+                resolve(api(originalRequestConfig));
+              },
+
+              onFailure: (error: AxiosError) => {
+                reject(error);
+              },
+            })
+          });
+        }
+
+        isRefreshing = true;
       }
 
       signOut();
